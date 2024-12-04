01e48620eaf64f1b02b0cf31b307180593b9e0b7
Ngocmanh1607 <127979809+Ngocmanh1607@users.noreply.github.com>
2024-12-04 15:26:51 +0700

    update

--- App.jsx
+++ App.jsx
@@ -3,7 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigator';
 import { Provider } from 'react-redux';
 import store from './src/store/store';
-
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs(true);
 function App() {
   return (
     <Provider store={store}>
